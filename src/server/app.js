2603e34b506245cf00b4486ead8a869fce52c8d5
John Desrosiers <john.desro@gmail.com>
2019-05-27 18:17:24 -0700

    Add cacheing to server routes

--- src/server/app.js
+++ src/server/app.js
@@ -5,6 +5,7 @@ const presentRepoList = require('../view-repo-list/present-repo-list')
 module.exports = app => {
   app.get('/api/repos', async (req, res) => {
     try {
+      res.set('Cache-Control', 'max-age=1800') // 30 minutes
       res.json(await presentRepoList.sortByLastPushAsc())
     } catch (error) {
       res.status(500).json(error)
@@ -12,6 +13,8 @@ module.exports = app => {
   })
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view-repo-list/index.html'))
+    res.sendFile(path.join(__dirname, '../view-repo-list/index.html'), {
+      maxAge: 360000, // one hour
+    })
   })
 }
